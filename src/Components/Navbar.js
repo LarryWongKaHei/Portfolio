f195f426bd6ee6eb7f5613d50ca58f711c9ea2bf
Maryhei <makemak123@gmail.com>
2023-03-16 15:19:05 +0000

    set up menu

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -6,16 +6,38 @@ export function Navbar (){
     const [showMenu, setShowMenu] = useState(false)
 
     function clickMenu (){
-        
+        setShowMenu(!showMenu)
     }
 
     return (
-        <div className="bg-gray-900 text-white py-5 sticky top-0 grid grid-cols-1">
+        <div className="bg-black text-white py-5 sticky top-0 flex justify-between">
             <div id="logoAndName" className="flex justify-start ml-5">
                 Larry
             </div>
             <div>
-                <GiHamburgerMenu size={25} className="fixed top-5 right-5"/>
+                <GiHamburgerMenu size={25} className="mr-5 hover:cursor-pointer" onClick={clickMenu}/>
+            </div>
+            <div id="menu" className={showMenu? "fixed left-0 top-16 bg-black h-full opacity-90 transition duration-500 ease-in-out" : "transition duration-300 ease-out fixed right-[100%]"}>
+                <ul className="mt-10 flex flex-col gap-20 mx-20">
+                    <li className="hover:text-gray-600 hover:cursor-pointer">
+                        Home
+                    </li>
+                    <li className="hover:text-gray-600 hover:cursor-pointer">
+                        Portfolio
+                    </li>
+                    <li className="hover:text-gray-600 hover:cursor-pointer">
+                        About
+                    </li>
+                    <li className="hover:text-gray-600 hover:cursor-pointer">
+                        Pricing
+                    </li>
+                    <li className="hover:text-gray-600 hover:cursor-pointer">
+                        Blog
+                    </li>
+                    <li className="hover:text-gray-600 hover:cursor-pointer">
+                        Contact
+                    </li>
+                </ul>
             </div>
         </div>
     )
