92e97e76329913c4c37f705abe27a72a97910227
Maryhei <makemak123@gmail.com>
2023-03-16 13:37:45 +0000

    Installed tailwind and tested

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>hi</h1>
+      <h1 className='text-5xl'>hi</h1>
     </div>
   );
 }
