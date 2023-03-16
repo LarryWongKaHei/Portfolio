0fa78a1438427066aa8c53bae2da860fc3193d28
Maryhei <makemak123@gmail.com>
2023-03-16 15:40:03 +0000

    Set up responsive navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -11,33 +11,30 @@ export function Navbar (){
 
     return (
         <div className="bg-black text-white py-5 sticky top-0 flex justify-between">
-            <div id="logoAndName" className="flex justify-start ml-5">
-                Larry
-            </div>
-            <div>
-                <GiHamburgerMenu size={25} className="mr-5 hover:cursor-pointer" onClick={clickMenu}/>
-            </div>
-            <div id="menu" className={showMenu? "fixed left-0 top-16 bg-black h-full opacity-90 transition duration-500 ease-in-out" : "transition duration-300 ease-out fixed right-[100%]"}>
-                <ul className="mt-10 flex flex-col gap-20 mx-20">
-                    <li className="hover:text-gray-600 hover:cursor-pointer">
-                        Home
-                    </li>
-                    <li className="hover:text-gray-600 hover:cursor-pointer">
-                        Portfolio
-                    </li>
-                    <li className="hover:text-gray-600 hover:cursor-pointer">
-                        About
-                    </li>
-                    <li className="hover:text-gray-600 hover:cursor-pointer">
-                        Pricing
-                    </li>
-                    <li className="hover:text-gray-600 hover:cursor-pointer">
-                        Blog
-                    </li>
-                    <li className="hover:text-gray-600 hover:cursor-pointer">
-                        Contact
-                    </li>
+            <div id="logoAndName" className="flex ml-5">Larry</div>
+            <div className="mx-auto">
+                <ul className="hidden md:flex gap-6 pr-10">
+                    <li className="hover:text-gray-600 cursor-pointer">Home</li>
+                    <li className="hover:text-gray-600 cursor-pointer">Portfolio</li>
+                    <li className="hover:text-gray-600 cursor-pointer">About</li>
+                    <li className="hover:text-gray-600 cursor-pointer">Pricing</li>
+                    <li className="hover:text-gray-600 cursor-pointer">Blog</li>
+                    <li className="hover:text-gray-600 cursor-pointer">Contact</li>
                 </ul>
+            </div>
+            <div id="menu" className={showMenu? "fixed left-0 top-16 bg-black h-full opacity-90 transition duration-500 ease-in-out" 
+            : "transition duration-300 ease-out fixed right-[100%]"}>
+                <ul className="mt-10 flex flex-col gap-20 mx-20">
+                    <li className="hover:text-gray-600 cursor-pointer">Home</li>
+                    <li className="hover:text-gray-600 cursor-pointer">Portfolio</li>
+                    <li className="hover:text-gray-600 cursor-pointer">About</li>
+                    <li className="hover:text-gray-600 cursor-pointer">Pricing</li>
+                    <li className="hover:text-gray-600 cursor-pointer">Blog</li>
+                    <li className="hover:text-gray-600 cursor-pointer">Contact</li>
+                </ul>
+            </div>
+            <div className="block md:hidden">
+                <GiHamburgerMenu size={25} className="mr-5 hover:cursor-pointer" onClick={clickMenu}/>
             </div>
         </div>
     )
