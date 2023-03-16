d6b4500c76928c6c5af0d8591ef299111e6b58b8
Maryhei <makemak123@gmail.com>
2023-03-16 14:27:47 +0000

    Set up a show menu hamburger button

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,12 +1,22 @@
 import { GiHamburgerMenu } from "react-icons/gi"
+import { useState } from "react"
 
 export function Navbar (){
+
+    const [showMenu, setShowMenu] = useState(false)
+
+    function clickMenu (){
+        
+    }
+
     return (
-        <div className="bg-gray-900 text-white py-5 sticky top-0 grid">
+        <div className="bg-gray-900 text-white py-5 sticky top-0 grid grid-cols-1">
             <div id="logoAndName" className="flex justify-start ml-5">
                 Larry
             </div>
-            <GiHamburgerMenu />
+            <div>
+                <GiHamburgerMenu size={25} className="fixed top-5 right-5"/>
+            </div>
         </div>
     )
 }
