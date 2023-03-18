f44a80feb4804458c9a2dce4e177e95e82648af5
Maryhei <makemak123@gmail.com>
2023-03-18 17:57:18 +0000

    Added learned more button

--- src/Components/MainIntro.js
+++ src/Components/MainIntro.js
@@ -3,11 +3,14 @@ import Photo from '../images/Photo.png'
 export function MainIntro (){
 
     return (
-        <div className="text-white bg-black md:flex ">
-            <div className='mt-20 text-center md:text-start md:h-40 md:fixed md:top-0 md:right-80 bg-black'>
-                <p className="py-3 text-xl">Hi, I'm Larry.</p>
-                <p className="py-3 text-5xl font-serif">I build solution.</p>
-                <p className="py-3">I'm a junior developer who tend to make a difference.</p>
+        <div className="text-white bg-black md:flex">
+            <div>
+                <div className='mt-32 text-center md:text-start ml-14 bg-black'>
+                    <p className="py-3 text-xl">Hi, I'm Larry.</p>
+                    <p className="py-3 text-5xl font-serif">I build solution.</p>
+                    <p className="py-3">I'm a junior developer who tend to make a difference.</p>
+                    <button className='text-center mt-3 text-orange-400 hover:text-orange-500 cursor-pointer'>Learn more ></button>
+                </div>
             </div>
             <div className=''>
                 {/* <p className="mt-5 ml-52">Meet my personal assistance Jarvis</p> */}
