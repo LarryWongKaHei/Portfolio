01df953bbbcc72d14d9b1c706b9e74a54ff6a792
Maryhei <makemak123@gmail.com>
2023-03-21 15:45:42 +0000

    Added email link

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,5 +1,19 @@
+import { HiOutlineMail } from "react-icons/hi"
+
 export function Contact(){
     return (
-        <h1 className="text-white">This is contact page</h1>
+        <div className="text-white">
+            <h1 className="text-center text-4xl font-extralight">To contact me</h1>
+            <div id="email" className="text-2xl mt-12 font-light flex flex-col">
+                <p className="mt-3 text-center">Via email</p>
+                <button className="mx-auto">
+                    <a href="mailto:makemak123@gmail.com" className="">
+                        < HiOutlineMail className="w-10 h-10 mt-4"/>
+                    </a>
+                </button>
+            </div>
+
+            
+        </div> 
     )
 }
