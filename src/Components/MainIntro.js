f93e52d774e7ed4a2e76962de475055eba528995
Maryhei <makemak123@gmail.com>
2023-03-24 12:41:38 +0000

    Modified the main intro to I build solutions from I build solution

--- src/Components/MainIntro.js
+++ src/Components/MainIntro.js
@@ -27,7 +27,7 @@ export function MainIntro (){
         <div className="text-white bg-black md:flex md:justify-between">
             <div className='mt-32 text-center md:text-start md:ml-10 bg-black'>
                 <p className="py-3 text-xl">Hi, I'm Larry.</p>
-                <p className="py-3 text-5xl font-serif">I build solution.</p>
+                <p className="py-3 text-5xl font-serif">I build solutions.</p>
                 <p className="py-3">I'm a junior developer who tend to make a difference.</p>
                 <button className='text-center mt-3 text-orange-400 hover:text-orange-500 cursor-pointer' onClick={toAbout}>Learn more ></button>
             </div>
