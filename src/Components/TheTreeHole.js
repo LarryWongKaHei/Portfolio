6060de9dc0014ca0af20f1506b8bce74cb610ff7
larry.westland <larry.wong@westlandTechnologies.co.uk>
2024-10-01 22:54:27 +0100

    Testing deploy github page

--- src/Components/TheTreeHole.js
+++ src/Components/TheTreeHole.js
@@ -12,9 +12,9 @@ export function TreeHole(){
             TreeHole is a metaphor in HongKong, it is used to describe a place or a person that you could rely on to share your secrets and release your emotions. and they are most likely a listener, that is why they are called TreeHole
             </h2>
             <img src={ Thetreehole } alt="TheTreeHole" className="mt-10"/>
-            <h1 className="mt-4 text-lg">
+            {/* <h1 className="mt-4 text-lg">
                 Link to the Page <a href="https://larrywongkahei.github.io/TheTreeHole/" className="text-blue-500 hover:text-blue-600">Here</a>
-            </h1>
+            </h1> */}
             <h1 className="mt-4 text-lg">
                 Link to the Github page <a href="https://github.com/larrywongkahei/TheTreeHole" className="text-blue-500 hover:text-blue-600">Here</a>
             </h1>
